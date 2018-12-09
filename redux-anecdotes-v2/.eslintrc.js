e3dd1c4c92088a3102ead29100177b41d7d79d3d
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-12-09 14:26:14 +0200

    6.1 done

---
+++ redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        'jest/globals': true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: ['react', 'jest'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'no-console': 0,
+        'react/prop-types': 0
+    }
+}
